25dcbdb2dd19779c2020520db075c57cdd9964c1
Roger Abelenda <roger.abelenda@abstracta.com.uy>
2019-07-02 17:40:25 -0300

    Fix test due to incorrect initialization of sampler
    Additionally, done some refactoring to improve readability and reduce code.

--- src/test/java/com/blazemeter/jmeter/hls/logic/HlsSamplerTest.java
+++ src/test/java/com/blazemeter/jmeter/hls/logic/HlsSamplerTest.java
@@ -9,19 +9,22 @@ import static org.mockito.Mockito.when;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import org.apache.jmeter.samplers.SampleResult;
 import org.json.simple.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class HlsSamplerTest {
 
   private static final String HEADERS = "headerKey1 : header11 header12 header13\n"
@@ -30,19 +33,26 @@ public class HlsSamplerTest {
   private static final String PLAYLIST_PATH = "/videos/DianaLaufenberg_2010X/video/600k.m3u8?preroll=Thousands&uniqueId=4df94b1d";
   private static final String BASE_URL = "http://www.mock.com";
   private static final String VIDEO_URL = BASE_URL + "/path";
+
   private HlsSampler sampler;
+
+  @Mock
   private Parser parserMock;
 
   @Before
-  public void setup() {
-    parserMock = Mockito.mock(Parser.class);
+  public void setup() throws IOException {
+    buildSampler();
+    setupParser();
+  }
+
+  private void buildSampler() {
     sampler = new HlsSampler();
     sampler.setURLData(VIDEO_URL);
     sampler.setResData("640x360");
     sampler.setNetworkData("1395723");
     sampler.setBandwidthType(BandwidthOption.CUSTOM);
     sampler.setResolutionType(ResolutionOption.CUSTOM);
-    sampler.setProtocol("https");
+    sampler.setProtocol("http");
     sampler.setPlaySecondsData("20");
     sampler.setVideoDuration(true);
     sampler.setParser(parserMock);
@@ -50,17 +60,13 @@ public class HlsSamplerTest {
     sampler.setResumeVideoStatus(true);
   }
 
-  @Test
-  public void testSample() throws Exception {
+  private void setupParser() throws IOException {
     Map<String, List<String>> headers = this.buildHeaders();
     setupMasterListParser(headers);
     setupPlayListParser(headers);
     setupFragmentParser(1, headers);
     setupFragmentParser(2, headers);
     setupFragmentParser(3, headers);
-    SampleResult result = sampler.sample(null);
-    SampleResult expected = buildExpectedSampleResult();
-    this.assertSampleResult(expected, result);
   }
 
   private Map<String, List<String>> buildHeaders() {
@@ -95,6 +101,21 @@ public class HlsSamplerTest {
         .thenReturn(buildDataRequest(url, headers, payload));
   }
 
+  private DataRequest buildDataRequest(String url, Map<String, List<String>> headers,
+      String payload) {
+    DataRequest respond = new DataRequest();
+    respond.setRequestHeaders("GET  " + url + "\n");
+    respond.setHeaders(headers);
+    respond.setResponse(payload);
+    respond.setResponseCode("200");
+    respond.setResponseMessage("OK");
+    respond.setContentType("application/json;charset=UTF-8");
+    respond.setSuccess(true);
+    respond.setSentBytes(payload.length());
+    respond.setContentEncoding("UTF-8");
+    return respond;
+  }
+
   private void setupPlayListParser(Map<String, List<String>> headers) throws IOException {
     String payload2 = "#EXTM3U\n"
         + "#EXT-X-TARGETDURATION:10\n"
@@ -118,81 +139,44 @@ public class HlsSamplerTest {
         .thenReturn(false);
   }
 
-  private void setupFragmentParser(int fragmentNumber,
-      Map<String, List<String>> headers) throws IOException {
-    String segmentUrl = this.buildSegmentUrl(fragmentNumber);
-    setupUrlParser(segmentUrl, headers, "chunk");
-  }
-
   //In this method we didn't use Arrays.asList since the test modifies
   // the list and list provided by the method is immutable.
   private List<DataFragment> buildFragments() {
-    List<DataFragment> fragments = new ArrayList<>();
-    fragments.add(new DataFragment("10",
-        this.buildSegmentUrl(1)));
-    fragments.add(new DataFragment("10",
-        this.buildSegmentUrl(2)));
-    fragments.add(new DataFragment("10",
-        this.buildSegmentUrl(3)));
-
-    return fragments;
-  }
-
-  private DataRequest buildDataRequest(String url, Map<String, List<String>> headers,
-      String payload) {
-    DataRequest respond = new DataRequest();
-    respond.setRequestHeaders("GET  " + url + "\n");
-    respond.setHeaders(headers);
-    respond.setResponse(payload);
-    respond.setResponseCode("200");
-    respond.setResponseMessage("OK");
-    respond.setContentType("application/json;charset=UTF-8");
-    respond.setSuccess(true);
-    respond.setSentBytes(payload.length());
-    respond.setContentEncoding("UTF-8");
-    return respond;
-  }
-
-  @SuppressWarnings("unchecked")
-  private JSONObject sampleResultToJson(SampleResult sample) {
-    JSONObject json = new JSONObject();
-    json.put("requestHeaders", sample.getRequestHeaders());
-    json.put("responseMessage", sample.getResponseMessage());
-    json.put("label", sample.getSampleLabel());
-    json.put("responseHeaders", sample.getResponseHeaders());
-    json.put("responseCode", sample.getResponseCode());
-    json.put("contentType", sample.getContentType());
-    json.put("dataEncoding", sample.getDataEncodingNoDefault());
-    json.put("children", Arrays.stream(sample.getSubResults()).map(this::sampleResultToJson)
-        .collect(Collectors.toList()));
-    return json;
-  }
-
-  private SampleResult buildExpectedSampleResult() {
-
-    SampleResult subResult = this
-        .buildSampleResult(BASE_URL + PLAYLIST_PATH,
-            "600k.m3u8?preroll=Thousands&uniqueId=4df94b1d");
-    subResult.addRawSubResult(this.buildSegmentSampleResult(1));
-    subResult.addRawSubResult(this.buildSegmentSampleResult(2));
-    subResult.addRawSubResult(this.buildSegmentSampleResult(3));
-
-    subResult.setResponseHeaders(HEADERS);
-
-    SampleResult expected = this.buildSampleResult(VIDEO_URL, "Test");
-    expected.setResponseHeaders(HEADERS);
-    expected.addRawSubResult(subResult);
-    return expected;
-  }
-
-  private SampleResult buildSegmentSampleResult(int segmentNumber) {
-    return this.buildSampleResult(
-        this.buildSegmentUrl(segmentNumber),
-        "Thousands-320k_" + segmentNumber + ".ts");
+    return IntStream.rangeClosed(1, 3)
+        .mapToObj(i -> new DataFragment("10", buildSegmentUrl(i)))
+        .collect(Collectors.toList());
   }
 
   private String buildSegmentUrl(int segmentNumber) {
     return "https://pb.tedcdn.com/bumpers/hls/video/in/Thousands-320k_" + segmentNumber + ".ts";
+  }
+
+  private void setupFragmentParser(int fragmentNumber,
+      Map<String, List<String>> headers) throws IOException {
+    String segmentUrl = buildSegmentUrl(fragmentNumber);
+    setupUrlParser(segmentUrl, headers, "chunk");
+  }
+
+  @Test
+  public void testSample() {
+    SampleResult result = sampler.sample(null);
+    SampleResult expected = buildExpectedSampleResult();
+    assertSampleResult(expected, result);
+  }
+
+  private SampleResult buildExpectedSampleResult() {
+    SampleResult subResult = buildSampleResult(BASE_URL + PLAYLIST_PATH,
+        "600k.m3u8?preroll=Thousands&uniqueId=4df94b1d");
+    subResult.addRawSubResult(buildSegmentSampleResult(1));
+    subResult.addRawSubResult(buildSegmentSampleResult(2));
+    subResult.addRawSubResult(buildSegmentSampleResult(3));
+
+    subResult.setResponseHeaders(HEADERS);
+
+    SampleResult expected = buildSampleResult(VIDEO_URL, "Test");
+    expected.setResponseHeaders(HEADERS);
+    expected.addRawSubResult(subResult);
+    return expected;
   }
 
   private SampleResult buildSampleResult(String url, String label) {
@@ -210,9 +194,29 @@ public class HlsSamplerTest {
     return sResult;
   }
 
+  private SampleResult buildSegmentSampleResult(int segmentNumber) {
+    return buildSampleResult(buildSegmentUrl(segmentNumber),
+        "Thousands-320k_" + segmentNumber + ".ts");
+  }
+
   private void assertSampleResult(SampleResult expected, SampleResult actual) {
     assertThat(sampleResultToJson(actual).toString())
         .isEqualTo(sampleResultToJson(expected).toString());
+  }
+
+  @SuppressWarnings("unchecked")
+  private JSONObject sampleResultToJson(SampleResult sample) {
+    JSONObject json = new JSONObject();
+    json.put("requestHeaders", sample.getRequestHeaders());
+    json.put("responseMessage", sample.getResponseMessage());
+    json.put("label", sample.getSampleLabel());
+    json.put("responseHeaders", sample.getResponseHeaders());
+    json.put("responseCode", sample.getResponseCode());
+    json.put("contentType", sample.getContentType());
+    json.put("dataEncoding", sample.getDataEncodingNoDefault());
+    json.put("children", Arrays.stream(sample.getSubResults()).map(this::sampleResultToJson)
+        .collect(Collectors.toList()));
+    return json;
   }
 
 }
