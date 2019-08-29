e5ea1def9f8f7dff4f97c371109e125af7836125
rpoleo <ricardo.poleo@cl.abstracta.us>
2019-08-29 18:56:40 -0400

    HLS-33: Added config on GitHub for Travis
    Added Config file for Travis Pipeline and, bash file to run the compatibility tests

---
+++ src/test/resources/jmeter/testJmeterTravis.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# For Travis
+# runs the plugin with the given JMeter version (through taurus)
+# environment variables: JMETER_VERSION, JVM_VERSION (optional, by default is 8)
+set -e
+
+JMETER_VERSION=${JMETER_VERSION:-$1}
+BZ_TOKEN=${BZ_TOKEN:-$2}
+
+echo "| Prepare testing for JMeter version $1 \n";
+JMETER_PATH=${project.basedir}/.jmeter/$JMETER_VERSION
+DEFAULT_JVM_VERSION=8
+JVM_VERSION=${JVM_VERSION:-$DEFAULT_JVM_VERSION}
+
+ERROR=0
+
+if [-z BZ_TOKEN]; then
+  bzt testJMeter.yaml -o m-o modules.cloud.token=$BZ_TOKEN || ERROR=$?
+  exit $ERROR
+fi
+
+[ "$JVM_VERSION" != "$DEFAULT_JVM_VERSION" ] && update-java-alternatives --set java-1.${JVM_VERSION}.0-openjdk-amd64
+bzt testJMeter.yaml -o modules.jmeter.version=$JMETER_VERSION -o modules.jmeter.path=$JMETER_PATH || ERROR=$?
+[ "$JVM_VERSION" != "$DEFAULT_JVM_VERSION" ] && update-java-alternatives --set java-1.${DEFAULT_JVM_VERSION}.0-openjdk-amd64
+exit $ERROR
