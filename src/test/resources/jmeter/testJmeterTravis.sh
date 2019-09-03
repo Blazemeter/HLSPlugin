3bcd11a38d2e1a1f2f66045b6a5100fedfb1c1cd
rpoleo <ricardo.poleo@cl.abstracta.us>
2019-09-02 21:41:55 -0400

    HLS-33: Fixed some issues, logic and cleaned the code

--- src/test/resources/jmeter/testJmeterTravis.sh
+++ src/test/resources/jmeter/testJmeterTravis.sh
@@ -7,19 +7,19 @@ set -e
 JMETER_VERSION=${JMETER_VERSION:-$1}
 BZ_TOKEN=${BZ_TOKEN:-$2}
 
-echo "| Prepare testing for JMeter version $1 \n";
 JMETER_PATH=${project.basedir}/.jmeter/$JMETER_VERSION
 DEFAULT_JVM_VERSION=8
 JVM_VERSION=${JVM_VERSION:-$DEFAULT_JVM_VERSION}
 
 ERROR=0
 
-if [-z BZ_TOKEN]; then
-  bzt testJMeter.yaml -o m-o modules.cloud.token=$BZ_TOKEN || ERROR=$?
-  exit $ERROR
+if [ -z "$BZ_TOKEN" ]
+then
+ [ "$JVM_VERSION" != "$DEFAULT_JVM_VERSION" ] && update-java-alternatives --set java-1.${JVM_VERSION}.0-openjdk-amd64
+ bzt testJMeter.yaml -o modules.jmeter.version=$JMETER_VERSION -o modules.jmeter.path=$JMETER_PATH || ERROR=$?
+ [ "$JVM_VERSION" != "$DEFAULT_JVM_VERSION" ] && update-java-alternatives --set java-1.${DEFAULT_JVM_VERSION}.0-openjdk-amd64
+else
+ bzt testJMeter.yaml -o modules.cloud.token=$BZ_TOKEN || ERROR=$?
 fi
 
-[ "$JVM_VERSION" != "$DEFAULT_JVM_VERSION" ] && update-java-alternatives --set java-1.${JVM_VERSION}.0-openjdk-amd64
-bzt testJMeter.yaml -o modules.jmeter.version=$JMETER_VERSION -o modules.jmeter.path=$JMETER_PATH || ERROR=$?
-[ "$JVM_VERSION" != "$DEFAULT_JVM_VERSION" ] && update-java-alternatives --set java-1.${DEFAULT_JVM_VERSION}.0-openjdk-amd64
 exit $ERROR
